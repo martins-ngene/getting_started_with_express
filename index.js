734691647abb9bad16d1dc18b17c2992a7f108e4
Martins Ngene <martinsngene9@gmail.com>
2023-01-06 15:39:41 +0100

    update: Add CORS and allow localhost:3000 access

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ const PORT = 3001
 
 const fs = require("fs")
 const path = require("path")
+const cors = require("cors")
 const pathToFile = path.resolve("./data.json")
 
 const getResouces = ()=>  JSON.parse(fs.readFileSync(pathToFile))
 
+// Enable CORS for localhost:3000
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+app.use(cors(corsOptions))
 app.get("/", (req, res)=>{
     res.send("Hello World")
 })
