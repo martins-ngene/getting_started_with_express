dcb580cf2f5df26dc94f57f30f4f5e7a7cb86a0c
Martins Ngene <martinsngene9@gmail.com>
2023-07-20 12:50:51 +0100

    new: start new express project

---
+++ README.md
@@ -0,0 +1,17 @@
+<!-- @format -->
+
+# Getting Started With Express js
+
+Express js is still one of the most preferred Node js frameworks in the javascript ecosysytem. In this tutorial, I have used some of the fundamental concepts of express js to build a todo list app. You will learn the basics and some advanced concepts in express js.
+
+Feel free to reach out to me to contribute to this repository or for any suggestions.
+
+![Typescript Banner](/assets/express.png)
+
+## Installations
+
+You can express from the npm js registry using npm, yarn or pnpm. Before installing, be sure to have Node js installed. For this project, I will be using npm:
+
+`npm install express`
+
+To read more about express js, visit: [TypeScript Installation Guide](https://expressjs.com/en/guide/routing.html)
--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-const express = require("express")
-const app = express()
-const PORT = 3001
+/** @format */
 
-const fs = require("fs")
-const path = require("path")
-const cors = require("cors")
-const pathToFile = path.resolve("./data.json")
+const express = require("express");
+const app = express();
+const PORT = 3001;
 
-const getResouces = ()=>  JSON.parse(fs.readFileSync(pathToFile))
+app.get("/", (req, res) => {
+  const response = {
+    ip: req.ip,
+    path: req.path,
+    method: req.method,
+    message: "Hello World, This is my server",
+  };
+  res.json(response);
+});
 
-// Enable CORS for localhost:3000
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
-app.use(cors(corsOptions))
-app.get("/", (req, res)=>{
-    res.send("Hello World")
-})
-
-app.get("/api/resources", (req, res)=>{
-    const resources = getResouces()
-    res.send(resources)
-})
-
-app.listen(PORT, ()=>{
-    console.log("Server is running on PORT " + PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT " + PORT);
+});
